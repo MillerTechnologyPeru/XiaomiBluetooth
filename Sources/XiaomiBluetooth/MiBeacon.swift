de7efad6d8dfec0a95ce592136e9976514ba16dd
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-11 23:51:48 -0700

    Add `MiBeacon.capability`

--- Sources/XiaomiBluetooth/MiBeacon.swift
+++ Sources/XiaomiBluetooth/MiBeacon.swift
@@ -24,6 +24,25 @@ public struct MiBeacon: Equatable, Hashable {
     
     /// Device Mac address
     public let address: BluetoothAddress?
+    
+    /// A set of options that represent the capabilities of a device.
+    public let capability: Capability?
+    
+    /// I/O capability, at present, only high-security BLE access will use this field, and only MiBeacon v5 version is supported.
+    ///
+    ///  It is only used before binding; when the binding is completed and events are reported (such as opening and closing), this field is no longer needed.
+    public let ioCapability: Capability.IO?
+}
+
+public extension MiBeacon {
+    
+    var isEncrypted: Bool {
+        frameControl.contains(.isEncrypted)
+    }
+    
+    var version: UInt8 {
+        frameControl.version
+    }
 }
 
 public extension MiBeacon {
@@ -51,10 +70,28 @@ public extension MiBeacon {
         }
         self.product = product
         self.frameCounter = data[4]
+        var index = 5
         if frameControl.contains(.macInclude) {
-            self.address = BluetoothAddress(littleEndian: BluetoothAddress(bytes: (data[5], data[6], data[7], data[8], data[9], data[10])))
+            let bytes = data.subdataNoCopy(in: index ..< index + 6)
+            index += 6
+            self.address = BluetoothAddress(littleEndian: BluetoothAddress(bytes: (bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5])))
         } else {
             self.address = nil
         }
+        if self.frameControl.contains(.capabilityInclude) {
+            let capability = Capability(rawValue: data[index])
+            self.capability = capability
+            index += 1
+            if capability.contains(.io) {
+                self.ioCapability = Capability.IO(rawValue: UInt16(littleEndian: UInt16(bytes: (data[index], data[index + 1]))))
+                index += 2
+            } else {
+                self.ioCapability = nil
+            }
+        } else {
+            self.capability = nil
+            self.ioCapability = nil
+        }
     }
 }
+
